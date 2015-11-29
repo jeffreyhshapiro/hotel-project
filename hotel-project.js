f8ed2ab413808381f91ab12ce542a35671b5bd13
unknown <jeffreyhshapiro@gmail.com>
2015-11-29 14:58:14 -0500

    added forms on body for streamlined user experience

--- hotel-project.js
+++ hotel-project.js
@@ -1,8 +1,33 @@
-$("#check-in").on("click", function(){
+var inputCodeDateIn = $("date-in");
+var inputCodeDateOut = $("date-out");
+
+$("#destination").click(function () {
+  alert("A list of cities will appear here");
+})
+
+$("#date").pickadate()
+$("#date-return").pickadate()
+
+
+
+// Date picker for check in and check out functions in modal
+/*$("#date-in").on("click", function(){
+  $('.datepicker').pickadate({
+    selectYears: true,
+    selectMonths: true
   $("#check-in").modal('show');
-})
+  $('.datepicker').pickadate({
+    selectYears: true,
+    selectMonths: true
+  })
+
+ $("#date-out").modal('show');
+  $(".modal-body").append(inputCodeDateOut);
+  $('.datepicker').pickadate({
+    selectYears: true,
+    selectMonths: true
+  })
+})*/
 
 
-$('.datepicker').pickadate({
-  
-})
+
