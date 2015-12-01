467e74b48f0c1aac574140e796737b6c0e1cf71f
unknown <jeffreyhshapiro@gmail.com>
2015-11-30 22:12:27 -0500

    made changes to layout and content in home page and layout page

--- hotel-layout.js
+++ hotel-layout.js
@@ -1,6 +1,13 @@
-$(".price").append(function(){
-  var price = Math.round(Math.random()*1000);
-  return price
-});
+$("document").ready(function(){
+
+  $(".price").append(function(){
+    var price = Math.round(Math.random()*1000);
+    return price
+  });
+
+  $(".hideListing").click(function(){
+    $(this).hide();
+  })
 
 
+})
